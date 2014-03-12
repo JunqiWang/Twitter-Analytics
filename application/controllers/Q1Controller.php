ffb035f7a5fd0e5b7ff5251205f3e35850d9964c
Ubuntu <ubuntu@ip-172-31-30-255.ec2.internal>
2014-03-12 19:52:33 +0000

    Fix Zend_Date issue

--- application/controllers/Q1Controller.php
+++ application/controllers/Q1Controller.php
@@ -7,7 +7,8 @@ class Q1Controller extends Zend_Controller_Action
     {
         $this->_helper->layout()->disableLayout(); 
         $this->_helper->viewRenderer->setNoRender(true);
-        $date = (new Zend_Date())->toString("YYYY-MM-dd HH:mm:ss");
+        $zenddate = new Zend_Date();
+        $date = $zenddate->toString("YYYY-MM-dd HH:mm:ss");
         echo "Dynamos,0000-0000-0000\n$date";
     }
 }
