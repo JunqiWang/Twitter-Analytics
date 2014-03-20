6de13efd29c6376776f7615ec387c1d6766f302d
JunqiWang <junqiw@andrew.cmu.edu>
2014-03-20 11:14:54 -0400

    need to cinfigure routing patterns rather than accessing real path

---
+++ q1/index.php
@@ -0,0 +1,4 @@
+<?php
+$d = new DateTime ();
+echo "Dynamos,0000-0000-0000\n" . $d->format ( "YYYY-MM-dd HH:mm:ss" ) . "\n";
+?>
--- q2/index(hbase).php
+++ q2/index(hbase).php
@@ -1,23 +1,23 @@
 <?php
-$GLOBALS["THRIFT_ROOT"] = "thriftphp";
-require_once ($GLOBALS["THRIFT_ROOT"] . "/src/Thrift.php");
+$GLOBALS ["THRIFT_ROOT"] = "thriftphp";
+require_once ($GLOBALS ["THRIFT_ROOT"] . "/src/Thrift.php");
 
-require_once ($GLOBALS["THRIFT_ROOT"] . "/lib/Thrift/Type/TMessageType.php");
-require_once ($GLOBALS["THRIFT_ROOT"] . "/lib/Thrift/Type/TType.php");
-require_once ($GLOBALS["THRIFT_ROOT"] . "/lib/Thrift/Exception/TException.php");
-require_once ($GLOBALS["THRIFT_ROOT"] . "/lib/Thrift/Exception/TTransportException.php");
-require_once ($GLOBALS["THRIFT_ROOT"] . "/lib/Thrift/Exception/TProtocolException.php");
-require_once ($GLOBALS["THRIFT_ROOT"] . "/lib/Thrift/Factory/TStringFuncFactory.php");
-require_once ($GLOBALS["THRIFT_ROOT"] . "/lib/Thrift/StringFunc/TStringFunc.php");
-require_once ($GLOBALS["THRIFT_ROOT"] . "/lib/Thrift/StringFunc/Core.php");
-require_once ($GLOBALS["THRIFT_ROOT"] . "/lib/Thrift/Transport/TTransport.php");
-require_once ($GLOBALS["THRIFT_ROOT"] . "/lib/Thrift/Transport/TSocket.php");
-require_once ($GLOBALS["THRIFT_ROOT"] . "/lib/Thrift/Transport/TBufferedTransport.php");
-require_once ($GLOBALS["THRIFT_ROOT"] . "/lib/Thrift/Protocol/TProtocol.php");
-require_once ($GLOBALS["THRIFT_ROOT"] . "/lib/Thrift/Protocol/TBinaryProtocol.php");
+require_once ($GLOBALS ["THRIFT_ROOT"] . "/lib/Thrift/Type/TMessageType.php");
+require_once ($GLOBALS ["THRIFT_ROOT"] . "/lib/Thrift/Type/TType.php");
+require_once ($GLOBALS ["THRIFT_ROOT"] . "/lib/Thrift/Exception/TException.php");
+require_once ($GLOBALS ["THRIFT_ROOT"] . "/lib/Thrift/Exception/TTransportException.php");
+require_once ($GLOBALS ["THRIFT_ROOT"] . "/lib/Thrift/Exception/TProtocolException.php");
+require_once ($GLOBALS ["THRIFT_ROOT"] . "/lib/Thrift/Factory/TStringFuncFactory.php");
+require_once ($GLOBALS ["THRIFT_ROOT"] . "/lib/Thrift/StringFunc/TStringFunc.php");
+require_once ($GLOBALS ["THRIFT_ROOT"] . "/lib/Thrift/StringFunc/Core.php");
+require_once ($GLOBALS ["THRIFT_ROOT"] . "/lib/Thrift/Transport/TTransport.php");
+require_once ($GLOBALS ["THRIFT_ROOT"] . "/lib/Thrift/Transport/TSocket.php");
+require_once ($GLOBALS ["THRIFT_ROOT"] . "/lib/Thrift/Transport/TBufferedTransport.php");
+require_once ($GLOBALS ["THRIFT_ROOT"] . "/lib/Thrift/Protocol/TProtocol.php");
+require_once ($GLOBALS ["THRIFT_ROOT"] . "/lib/Thrift/Protocol/TBinaryProtocol.php");
 
-require_once ($GLOBALS["THRIFT_ROOT"] . "/packages/Hbase/Hbase.php");
-require_once ($GLOBALS["THRIFT_ROOT"] . "/packages/Hbase/Types.php");
+require_once ($GLOBALS ["THRIFT_ROOT"] . "/packages/Hbase/Hbase.php");
+require_once ($GLOBALS ["THRIFT_ROOT"] . "/packages/Hbase/Types.php");
 
 use Thrift\Transport\TSocket;
 use Thrift\Transport\TBufferedTransport;
@@ -27,35 +27,22 @@ use Hbase\HbaseClient;
 use Hbase\ColumnDescriptor;
 use Hbase\Mutation;
 
-    public static $table = "uidtime2ids";
+$socket = new TSocket ( "localhost" );
+$socket->setSendTimeout ( 2000 );
+$socket->setRecvTimeout ( 4000 );
+$transport = new TBufferedTransport ( $socket );
+$protocol = new TBinaryProtocol ( $transport );
+$client = new HbaseClient ( $protocol );
 
-    public static $dummy_attributes = array();
+$transport->open ();
 
-    private $tee;
-
-        
-        $socket = new TSocket("localhost");
-        $socket->setSendTimeout(2000);
-        $socket->setRecvTimeout(4000);
-        $transport = new TBufferedTransport($socket);
-        $protocol = new TBinaryProtocol($transport);
-        $client = new HbaseClient($protocol);
-        
-        $transport->open();
-        
-        $rowkey = $this->_request->getParam("userid") . $this->_request->getParam("tweettime");
-        $rowResult = $client->get(self::$table, $rowkey, "ids", self::$dummy_attributes);
-        $this->printBack($rowResult);
-        
-        $transport->close();
-    }
-
-    private function printBack($rowresult)
-    {
-        echo ("Dynamos,0000-0000-0000\n");
-        foreach ($rowresult as $rs) {
-            echo ("$rs->value\n");
-        }
-    }
+$rowkey = $_GET ["userid"] . $_GET ["tweettime"];
+$rowResult = $client->get ( "uidtime2ids", $rowkey, "ids", array () );
+echo ("Dynamos,0000-0000-0000\n");
+foreach ( $rowresult as $rs ) {
+	echo ("$rs->value\n");
 }
+
+$transport->close ();
+
 ?>
