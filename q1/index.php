6fe087fbb6fb8ff5ff184adfee2432f83ec4900b
JunqiWang <junqiw@andrew.cmu.edu>
2014-03-20 11:59:39 -0400

    q1

--- q1/index.php
+++ q1/index.php
@@ -1,4 +1,5 @@
 <?php
+date_default_timezone_set ( "GMT" );
 $d = new DateTime ();
-echo "Dynamos,0000-0000-0000\n" . $d->format ( "YYYY-MM-dd HH:mm:ss" ) . "\n";
+echo "Dynamos,0000-0000-0000\n" . $d->format ( "Y-m-d H:m:s" ) . "\n";
 ?>
