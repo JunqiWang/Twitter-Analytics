fec2265895e89a41827f4905adbba9d4fa712466
Junqi Wang <junqi.wang@live.com>
2014-03-20 23:08:38 -0400

    Update index.php

--- q1/index.php
+++ q1/index.php
@@ -2,6 +2,6 @@
 
 $d = new DateTime ();
 $d->setTimezone(new DateTimeZone('America/New_York'));
-echo "Dynamos,2427-6611-7783\n" . $d->format ( "Y-m-d H:m:s" ) . "\n";
+echo "Dynamos,2427-6611-7783\n" . $d->format ( "Y-m-d H:m:s" );
 
 ?>
