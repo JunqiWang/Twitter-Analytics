f95bca2bde89f54fdc7d0d8da2f3b9ba49bfcd6a
Ubuntu <ubuntu@ip-172-31-29-68.ec2.internal>
2014-03-22 06:43:48 +0000

    New

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
 <?php
-define('RUNNING_FROM_ROOT', true);
-include 'public/index.php';
+$TEAM_ID = "Dynamos";
+$AWS_ACC_ID = "9392-2385-3384";
+$url = $_GET['url'];
+
+function startsWith($haystack, $needle) {
+  return $needle === "" || strpos($haystack, $needle) === 0;
+}
+
+/* All Q1 code goes here */
+if ($url == "q1") {
+  $date = new DateTime();
+  $date_string = $date->format("Y-m-d H:i:s");
+  echo "$TEAM_ID,$AWS_ACC_ID\n$date_string";
+}
+
+/* All Q2 code goes here */
+else if (startsWith($url, "q2")) {
+  echo "q2";
+}
+
+?>
