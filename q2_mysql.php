e57ecb4a8cb6ccd9f97cf968dc6be755ae7d713e
chengzh1 <cheng.zhang@sv.cmu.edu>
2014-03-24 12:08:17 -0400

    change of q2_mysql format

--- q2_mysql.php
+++ q2_mysql.php
@@ -3,8 +3,8 @@
 // deal with get parameter
 $userid = $_GET ['userid'];
 $time = $_GET ['tweet_time'];
-$formatedTime = date ( "Y-m-d-h-i-s", strtotime ( $time ) );
-$user_time = $userid . '-' . $formatedTime;
+
+$user_time = $userid . '+' . $time;
 
 // connect to sql
 $con = mysqli_connect ( "localhost", "", "", "test" );
@@ -12,7 +12,7 @@ $con = mysqli_connect ( "localhost", "", "", "test" );
 $query = "SELECT * FROM tweet WHERE user_time = \"" . $user_time . "\";";
 $result = mysqli_query ( $con, $query );
 
-echo ("Dynamos,1234-5678-9012\n");
+echo ("Dynamos,2427-6611-7783\n");
 while ( $row = mysqli_fetch_array ( $result ) ) {
 	echo $row ['tweet'] . "\n";
 }
